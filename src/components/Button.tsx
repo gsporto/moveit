fc02b588b32df2d7649f3c9e7e7c08c27be0663f
Gabriel Porto <gabriel_porto@outlook.com>
2021-02-22 22:48:54 -0300

    the state of my life right now ⚛️

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,12 +1,25 @@
+import { useState } from "react";
+
 interface ButtonProps {
-    color: string;
-    children: string;
+  color: string;
+  children: string;
 }
 
 export function Button(props: ButtonProps) {
-    return ( 
-        <button type="button" style={{backgroundColor:props.color}}>
-            {props.children}
-        </button>
-    )
+  const [counter, setCounter] = useState<number>(1);
+
+  function increment() {
+    setCounter((state) => state + 1);
+  }
+
+  return (
+    <button
+      type="button"
+      style={{ backgroundColor: props.color }}
+      onClick={increment}
+    >
+      {props.children}
+      <strong>{counter}</strong>
+    </button>
+  );
 }
