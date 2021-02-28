12f85b2354ec87d39ba6f16f586d442a9380b833
Gabriel Porto <gabriel_porto@outlook.com>
2021-02-28 17:13:27 -0300

    that was a long day 🥱

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,29 @@
 import Head from "next/head";
+import { CompletedChallenges } from "../components/CompletedChallenges";
+import { Coutndown } from "../components/Countdown";
 import { ExperienceBar } from "../components/ExperienceBar";
+import { Profile } from "../components/Profile";
+
+import styles from "../styles/pages/Home.module.css";
 
 export default function Home() {
   return (
-    <div className="container">
-      <ExperienceBar></ExperienceBar>
-    </div>
+    <>
+      <Head>
+        <title>Inicio | move.it</title>
+      </Head>
+      <div className={styles.container}>
+        <ExperienceBar></ExperienceBar>
+
+        <section>
+          <div>
+            <Profile />
+            <CompletedChallenges />
+            <Coutndown />
+          </div>
+          <div></div>
+        </section>
+      </div>
+    </>
   );
 }
